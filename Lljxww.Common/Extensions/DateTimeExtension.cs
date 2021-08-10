dfb21c184fc339d3967e61f3f8512a88a87c2537
liangjw <liangjw1224@foxmail.com>
2021-08-10 22:10:25 +0800

    添加一些扩展方法

---
+++ Lljxww.Common/Extensions/DateTimeExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lljxww.Common.Extensions
+{
+    public static class DateTimeExtension
+    {
+        private static readonly DateTime BASE_UTC_TIME = new(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        //TODO 转移到Common
+        /// <summary>
+        /// 将时间转换为Unix时间戳
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static string ToUnixTimestamp(this DateTime dateTime)
+        {
+            string timestamp = (dateTime - BASE_UTC_TIME).TotalSeconds.ToString();
+            return timestamp.Split('.')[0];
+        }
+    }
+}
---
+++ Lljxww.Common/Extensions/ExpressionExtension.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Lljxww.Common.Extensions
+{
+    public static class ExpressionExtension
+    {
+        /// <summary>
+        /// Lambda表达式拼接
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <param name="merge"></param>
+        /// <returns></returns>
+        public static Expression<T> Compose<T>(this Expression<T> first, Expression<T> second, Func<Expression, Expression, Expression> merge)
+        {
+            Dictionary<ParameterExpression, ParameterExpression> map = first.Parameters.Select((f, i) => new { f, s = second.Parameters[i] }).ToDictionary(p => p.s, p => p.f);
+            Expression secondBody = ParameterRebinder.ReplaceParameters(map, second.Body);
+            return Expression.Lambda<T>(merge(first.Body, secondBody), first.Parameters);
+        }
+
+        /// <summary>
+        /// and扩展
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <returns></returns>
+        public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> first, Expression<Func<T, bool>> second)
+        {
+            return first.Compose(second, Expression.And);
+        }
+
+        /// <summary>
+        /// or扩展
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <returns></returns>
+        public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> first, Expression<Func<T, bool>> second)
+        {
+            return first.Compose(second, Expression.Or);
+        }
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ParameterRebinder : ExpressionVisitor
+    {
+        private readonly Dictionary<ParameterExpression, ParameterExpression> map;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="map"></param>
+        public ParameterRebinder(Dictionary<ParameterExpression, ParameterExpression> map)
+        {
+            this.map = map ?? new Dictionary<ParameterExpression, ParameterExpression>();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="map"></param>
+        /// <param name="exp"></param>
+        /// <returns></returns>
+        public static Expression ReplaceParameters(Dictionary<ParameterExpression, ParameterExpression> map, Expression exp)
+        {
+            return new ParameterRebinder(map).Visit(exp);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        protected override Expression VisitParameter(ParameterExpression p)
+        {
+            if (map.TryGetValue(p, out ParameterExpression? replacement))
+            {
+                p = replacement;
+            }
+
+            return base.VisitParameter(p);
+        }
+    }
+}
