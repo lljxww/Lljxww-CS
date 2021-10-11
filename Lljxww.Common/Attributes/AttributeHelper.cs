1224ce7f6d279b84fc3a37bf9a42d344300a14c7
liangjw1224 <liangjw1224@foxmail.com>
2021-10-11 20:43:58 +0800

    完善AttributeHelper

--- Lljxww.Common/Attributes/AttributeHelper.cs
+++ Lljxww.Common/Attributes/AttributeHelper.cs
@@ -38,5 +38,38 @@ namespace Lljxww.Common.Attributes
 
             return results;
         }
+        
+        public static TTarget? GetFieldAttribute<TSource, TTarget>(string propertyName) where TTarget : Attribute
+        {
+            var prop = typeof(TSource).GetField(propertyName);
+            TTarget? attr = prop?.GetCustomAttribute<TTarget>(false);
+
+            if (attr is null)
+            {
+                return default;
+            }
+            else
+            {
+                return attr;
+            }
+        }
+
+        public static IEnumerable<TTarget> GetFieldAttributies<TSource, TTarget>() where TTarget : Attribute
+        {
+            var props = typeof(TSource).GetFields();
+
+            IList<TTarget> results = new List<TTarget>();
+
+            foreach (var prop in props)
+            {
+                TTarget? attr = prop.GetCustomAttribute<TTarget>(false);
+                if (attr is not null)
+                {
+                    results.Add(attr);
+                }
+            }
+
+            return results;
+        }
     }
 }
