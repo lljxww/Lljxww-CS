9ca1bff0a77461ce269a1c43ebf08bea3abacaf2
liangjw <liangjw1224@foxmail.com>
2021-11-12 21:35:19 +0800

    更新NuGet引用

---
+++ Lljxww.Common/WebApiCaller/DynamicClientBuilder.cs
@@ -0,0 +1,23 @@
+﻿using Lljxww.Common.WebApiCaller.Models.Config;
+using Microsoft.Extensions.Options;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace Lljxww.Common.WebApiCaller
+{
+    public class DynamicClientBuilder
+    {
+        private readonly ApiCallerConfig _config;
+
+        public DynamicClientBuilder(IOptions<ApiCallerConfig> apiCallerConfigOption)
+        {
+            _config = apiCallerConfigOption.Value;
+        }
+
+        public void Init()
+        {
+            AssemblyName? assemblyName = new("Lljxww.Common.WebApiCaller.DynamicClient");
+            AssemblyBuilder builder = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.RunAndCollect);
+        }
+    }
+}
