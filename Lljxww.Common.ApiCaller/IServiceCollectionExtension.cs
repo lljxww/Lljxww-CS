85f6227890642d1749542c766aff02ae4d9f5a8c
liangjw <liangjw1224@foxmail.com>
2022-02-17 14:57:06 +0800

    添加Caller配置的初始化方式

--- Lljxww.Common.ApiCaller/IServiceCollectionExtension.cs
+++ Lljxww.Common.ApiCaller/IServiceCollectionExtension.cs
@@ -7,6 +7,12 @@ namespace Lljxww.Common.ApiCaller
 {
     public static class IServiceCollectionExtension
     {
+        /// <summary>
+        /// 使用指定的配置节初始化Caller配置
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
         public static IServiceCollection ConfigureCaller(this IServiceCollection services, IConfiguration configuration)
         {
             services.Configure<ApiCallerConfig>(configuration);
@@ -16,13 +22,29 @@ namespace Lljxww.Common.ApiCaller
             return services;
         }
 
-        public static IServiceCollection ConfigureCaller(this IServiceCollection services)
+        /// <summary>
+        /// 使用指定的配置文件初始化Caller配置
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="jsonFileName"></param>
+        /// <returns></returns>
+        public static IServiceCollection ConfigureCaller(this IServiceCollection services, string jsonFileName)
         {
             IConfigurationRoot configuration = new ConfigurationBuilder()
-                .AddJsonFile("caller.json")
+                .AddJsonFile(jsonFileName)
                 .Build();
 
             return ConfigureCaller(services, configuration);
+        }
+
+        /// <summary>
+        /// 使用默认的配置文件初始化Caller配置
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection ConfigureCaller(this IServiceCollection services)
+        {
+            return ConfigureCaller(services, "caller.json");
         }
     }
 }
